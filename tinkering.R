e4f3695d0ce3e8139f5968fbb2636f1d94ad468b
shraddha-1 <67503169+shraddha-1@users.noreply.github.com>
2021-10-03 11:46:16 +0530

    Tinkering

---
+++ tinkering.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+library(dslabs)
+data(murders)
+
+murders %>% ggplot() +
+  geom_point(aes(x = population/10^6, y = total))
+
+# add points layer to predefined ggplot object
+p <- ggplot(data = murders)
+p + geom_point(aes(population/10^6, total))
+
+# add text layer to scatterplot
+p + geom_point(aes(population/10^6, total)) +
+  geom_text(aes(population/10^6, total, label = abb))
+#AES behaviour
+# no error from this call
+p_test <- p + geom_text(aes(population/10^6, total, label = abb))
+
+# error - "abb" is not a globally defined variable and cannot be found outside of aes
+p_test <- p + geom_text(aes(population/10^6, total), label = abb)
+p + geom_point(aes(population/10^6, total), size = 3) +
+  geom_text(aes(population/10^6, total, label = abb))
+
+# move text labels slightly to the right
+p + geom_point(aes(population/10^6, total), size = 3) +
+  geom_text(aes(population/10^6, total, label = abb), nudge_x = 1)
+
+# simplify code by adding global aesthetic
+p <- murders %>% ggplot(aes(population/10^6, total, label = abb))
+p + geom_point(size = 3) +
+  geom_text(nudge_x = 1.5)
+
+# local aesthetics override global aesthetics
+p + geom_point(size = 3) +
+  geom_text(aes(x = 10, y = 800, label = "Hello there!"))
