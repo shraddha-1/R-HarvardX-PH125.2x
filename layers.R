d43847052664683337e9d2d551820050d5b0265d
shraddha-1 <67503169+shraddha-1@users.noreply.github.com>
2021-10-03 11:33:46 +0530

    Adding layers to a plot & Example of aes behavior

---
+++ layers.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(dslabs)
+data(murders)
+
+murders %>% ggplot() +
+  geom_point(aes(x = population/10^6, y = total))
+
+# add points layer to predefined ggplot object
+p <- ggplot(data = murders)
+p + geom_point(aes(population/10^6, total))
+
+# add text layer to scatterplot
+p + geom_point(aes(population/10^6, total)) +
+  geom_text(aes(population/10^6, total, label = abb))
+#AES behaviour
+# no error from this call
+p_test <- p + geom_text(aes(population/10^6, total, label = abb))
+
+# error - "abb" is not a globally defined variable and cannot be found outside of aes
+p_test <- p + geom_text(aes(population/10^6, total), label = abb)
