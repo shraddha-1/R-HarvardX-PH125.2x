cde8ec0e50d4cd9ce75d56f7ccb743ded481341b
shraddha-1 <67503169+shraddha-1@users.noreply.github.com>
2021-10-04 20:43:06 +0530

    use of facet_grid

---
+++ gapminder_facet.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+library(dslabs)
+data(gapminder)
+
+gapminder %>%
+  filter(year == 2015 & country %in% c("Sri Lanka","Turkey")) %>%
+  select(country, infant_mortality)
+ds_theme_set()    # set plot theme
+filter(gapminder, year == 1962) %>%
+  ggplot(aes(fertility, life_expectancy)) +
+  geom_point()
+
+# add color as continent
+filter(gapminder, year == 1962) %>%
+  ggplot(aes(fertility, life_expectancy, color = continent)) +
+  geom_point()
+filter(gapminder, year %in% c(1962, 2012)) %>%
+  ggplot(aes(fertility, life_expectancy, col = continent)) +
+  geom_point() +
+  facet_grid(continent ~ year)
+
+# facet by year only
+filter(gapminder, year %in% c(1962, 2012)) %>%
+  ggplot(aes(fertility, life_expectancy, col = continent)) +
+  geom_point() +
+  facet_grid(. ~ year)
+
+# facet by year, plots wrapped onto multiple rows
+years <- c(1962, 1980, 1990, 2000, 2012)
+continents <- c("Europe", "Asia")
+gapminder %>%
+  filter(year %in% years & continent %in% continents) %>%
+  ggplot(aes(fertility, life_expectancy, col = continent)) +
+  geom_point() +
+  facet_wrap(~year)
