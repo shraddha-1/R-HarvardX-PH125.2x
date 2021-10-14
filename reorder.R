e1f39427a607f2702cc4df09f3a3c38b04578e40
shraddha-1 <67503169+shraddha-1@users.noreply.github.com>
2021-10-14 21:07:28 +0530

    Add files via upload

---
+++ reorder.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+library(dslabs)
+data(gapminder)
+# add dollars per day variable
+gapminder <- gapminder %>%
+  mutate(dollars_per_day = gdp/population/365)
+
+# number of regions
+length(levels(gapminder$region))
+
+# boxplot of GDP by region in 1970
+past_year <- 1970
+p <- gapminder %>%
+  filter(year == past_year & !is.na(gdp)) %>%
+  ggplot(aes(region, dollars_per_day))
+p + geom_boxplot()
+
+# rotate names on x-axis
+p + geom_boxplot() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+# by default, factor order is alphabetical
+fac <- factor(c("Asia", "Asia", "West", "West", "West"))
+levels(fac)
+
+# reorder factor by the category means
+value <- c(10, 11, 12, 6, 4)
+fac <- reorder(fac, value, FUN = mean)
+levels(fac)
